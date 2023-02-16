3f33c0fa98f82802fbc7bccea3989eac3c7bc898
Mauritz Sverredal <user@example.com>
2023-02-16 20:24:44 +0100

    Add basic export_to_fen unit test

--- chess_test.ts
+++ chess_test.ts
@@ -1,8 +1,14 @@
 import { assertEquals } from "https://deno.land/std@0.177.0/testing/asserts.ts";
-import { position_from_fen } from './chess.ts'
+import { export_to_fen, position_from_fen } from './chess.ts'
 
 Deno.test("basic_fen", () => {
     const board = position_from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
     assertEquals(board.pieces.length, 32)
     assertEquals(board.en_passant_square, null)
+})
+
+Deno.test("export_starting_position", () => {
+    const starting_position_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+    const board = position_from_fen(starting_position_fen)
+    assertEquals(export_to_fen(board), starting_position_fen)
 })
