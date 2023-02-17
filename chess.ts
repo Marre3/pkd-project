eb2d0f9389ba339327a6494ecddb1e198b13464d
Mauritz Sverredal <user@example.com>
2023-02-17 03:26:09 +0100

    Small fix for function draw()

--- chess.ts
+++ chess.ts
@@ -395,7 +395,7 @@ function draw(state: BoardState): void {
     for (let y = 8; y >= 1; --y) {
         let s = ""
         for (let x = 1; x <= 8; ++x) {
-            const p = get_piece_by_square(make_coordinates(x, y), board)
+            const p = get_piece_by_square(make_coordinates(x, y), state)
 
             s = s + " " + get_letter_by_piece(p) + " "
         }
