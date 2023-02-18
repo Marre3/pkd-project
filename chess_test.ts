82cb17cd0832c533ddf16806d98ef9d210b1c078
Mauritz Sverredal <user@example.com>
2023-02-18 03:32:35 +0100

    Add test test_coordinates_to_notation

--- chess_test.ts
+++ chess_test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "https://deno.land/std@0.177.0/testing/asserts.ts";
-import { export_to_fen, get_default_board, get_legal_moves, position_from_fen } from './chess.ts'
+import { coordinates_to_notation, export_to_fen, get_default_board, get_legal_moves, make_coordinates, position_from_fen } from './chess.ts'
 
 Deno.test("basic_fen", () => {
     const board = position_from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
@@ -16,4 +16,17 @@ Deno.test("export_starting_position", () => {
 Deno.test("starting_position_number_of_moves", () => {
     const board = get_default_board()
     assertEquals(get_legal_moves(board).length, 20)
+})
+
+Deno.test("test_coordinates_to_notation", () => {
+    assertEquals(coordinates_to_notation(make_coordinates(1, 1)), "a1")
+    assertEquals(coordinates_to_notation(make_coordinates(1, 8)), "a8")
+    assertEquals(coordinates_to_notation(make_coordinates(8, 8)), "h8")
+    assertEquals(coordinates_to_notation(make_coordinates(8, 1)), "h1")
+    assertEquals(coordinates_to_notation(make_coordinates(3, 4)), "c4")
+    assertEquals(coordinates_to_notation(make_coordinates(2, 7)), "b7")
+    assertEquals(coordinates_to_notation(make_coordinates(4, 4)), "d4")
+    assertEquals(coordinates_to_notation(make_coordinates(5, 2)), "e2")
+    assertEquals(coordinates_to_notation(make_coordinates(6, 5)), "f5")
+    assertEquals(coordinates_to_notation(make_coordinates(7, 3)), "g3")
 })
