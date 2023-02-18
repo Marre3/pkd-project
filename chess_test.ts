628c6dc4fe774e020847677fd489f3bfb1f4fb83
swanis <me@swan.is>
2023-02-18 03:50:37 +0100

    convert moves to algebraic notation + pawn capture logic

--- chess_test.ts
+++ chess_test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "https://deno.land/std@0.177.0/testing/asserts.ts";
-import { export_to_fen, get_default_board, get_legal_moves, position_from_fen } from './chess.ts'
+import { export_to_fen, get_default_board, get_legal_moves, position_from_fen, move_to_algebraic_notation, make_coordinates, Piece } from './chess.ts'
 
 Deno.test("basic_fen", () => {
     const board = position_from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
@@ -16,4 +16,15 @@ Deno.test("export_starting_position", () => {
 Deno.test("starting_position_number_of_moves", () => {
     const board = get_default_board()
     assertEquals(get_legal_moves(board).length, 20)
+})
+
+Deno.test("starting_position_move_notation", () => {
+    const board = get_default_board()
+    const nf3 = move_to_algebraic_notation(board, { from: make_coordinates(7, 1), to: make_coordinates(6, 3), piece_type: Piece.Knight, is_castling: false, is_en_passant: false })
+    const d4 = move_to_algebraic_notation(board, { from: make_coordinates(4, 2), to: make_coordinates(4, 4), piece_type: Piece.Pawn, is_castling: false, is_en_passant: false })
+    const d5_invalid = move_to_algebraic_notation(board, { from: make_coordinates(4, 7), to: make_coordinates(4, 5), piece_type: Piece.Pawn, is_castling: false, is_en_passant: false })
+
+    assertEquals(nf3, "Nf3")
+    assertEquals(d4, "d4")
+    assertEquals(d5_invalid, null)
 })
