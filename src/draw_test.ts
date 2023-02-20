4f5242ec060534dca68ff71aead34218b333ce84
Mauritz Sverredal <user@example.com>
2023-02-20 14:58:42 +0100

    Move draw() to draw.ts

--- src/draw_test.ts
+++ src/draw_test.ts
@@ -1,5 +1,5 @@
 import { get_default_board } from "./chess.ts";
-import { draw } from "./main.ts";
+import { draw } from "./draw.ts";
 
 Deno.test("test_draw", () => {
     const board = get_default_board()
