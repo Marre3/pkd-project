9a536e2dfdee687f170e97c240d8d878e5b9a117
Mauritz Sverredal <user@example.com>
2023-02-20 14:58:42 +0100

    Move draw() to draw.ts

---
+++ src/draw.ts
@@ -0,0 +1,16 @@
+import { BoardState } from "./game_types.ts"
+import { make_coordinates } from "./coordinates.ts"
+import { get_piece_by_square } from "./board.ts";
+import { get_letter_by_piece } from "./notation.ts";
+
+export function draw(state: BoardState): void {
+    for (let y = 8; y >= 1; --y) {
+        let s = ""
+        for (let x = 1; x <= 8; ++x) {
+            const p = get_piece_by_square(make_coordinates(x, y), state)
+
+            s = s + " " + get_letter_by_piece(p) + " "
+        }
+        console.log(s)
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,8 @@
-import { BoardState } from "./game_types.ts"
-import { make_coordinates } from "./coordinates.ts"
-import { get_piece_by_square } from "./board.ts";
-import { get_default_board } from "./chess.ts"
+import { get_default_board } from "./chess.ts";
+import { draw } from "./draw.ts";
 import { get_legal_moves } from "./moves.ts";
-import { get_letter_by_piece, move_to_algebraic_notation } from "./notation.ts";
+import { move_to_algebraic_notation } from "./notation.ts";
 
-export function draw(state: BoardState): void {
-    for (let y = 8; y >= 1; --y) {
-        let s = ""
-        for (let x = 1; x <= 8; ++x) {
-            const p = get_piece_by_square(make_coordinates(x, y), state)
-
-            s = s + " " + get_letter_by_piece(p) + " "
-        }
-        console.log(s)
-    }
-}
 const board = get_default_board()
 
 draw(board)
