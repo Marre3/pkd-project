fe212d8bc1eba476fd3e557f0b567ca1092b1739
Mauritz Sverredal <user@example.com>
2023-02-20 15:05:02 +0100

    Add playable text based game prototype

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { get_default_board } from "./chess.ts";
 import { draw } from "./draw.ts";
-import { get_legal_moves } from "./moves.ts";
+import { Color } from "./game_types.ts";
+import { apply_move, get_legal_moves, is_checkmate } from "./moves.ts";
 import { move_to_algebraic_notation } from "./notation.ts";
 
-const board = get_default_board()
+let board = get_default_board()
+while (! is_checkmate(board, board.turn)) {
+    draw(board)
+    const input = prompt("move?")
+    const moves = get_legal_moves(board)
 
-draw(board)
+    const selected_move = moves.find((m) => move_to_algebraic_notation(board, m) === input)
 
-for (const move of get_legal_moves(board)) {
-    console.log(move_to_algebraic_notation(board, move))
+    if (selected_move !== undefined) {
+        board = apply_move(board, selected_move)
+    } else {
+        console.log(`Invalid move ${input}, available moves:`)
+        console.log(moves.map((move) => move_to_algebraic_notation(board, move)).join(", "))
+    }
 }
+console.log(`Game over, ${board.turn === Color.White ? "White" : "Black"} wins`)
