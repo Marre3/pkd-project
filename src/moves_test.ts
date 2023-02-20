23030dfce179f82b67fbedf18df5ee5a234981c9
Mauritz Sverredal <user@example.com>
2023-02-20 15:48:22 +0100

    Add is_stalemate and is_game_over

--- src/moves_test.ts
+++ src/moves_test.ts
@@ -1,9 +1,10 @@
 import { assert } from "https://deno.land/std@0.177.0/testing/asserts.ts";
 
 import { Color, Move } from "./game_types.ts"
-import { is_check, is_checkmate, is_self_check } from "./moves.ts";
+import { is_check, is_checkmate, is_self_check, is_stalemate } from "./moves.ts";
 import { position_from_fen } from "./fen.ts";
 import { other_color } from "./board.ts";
+import { is_game_over } from "./game.ts";
 
 Deno.test("is_check_bogo_indian", () => {
     const board = position_from_fen("rnbqk2r/pppp1ppp/4pn2/8/1bPP4/5N2/PP2PPPP/RNBQKB1R w KQkq - 3 4")
@@ -33,7 +34,14 @@ Deno.test("is_self_check_h5_scholars", () => {
     assert(is_self_check(board, move))
 })
 
-Deno.test("is_checkmate_scholars", () => {
+Deno.test("checkmate_scholars", () => {
     const board = position_from_fen("r1bqkb1r/pppp1Qpp/2n2n2/4p3/2B1P3/8/PPPP1PPP/RNB1K1NR b KQkq - 0 4")
-    assert(is_checkmate(board, Color.Black))
+    assert(is_checkmate(board))
+    assert(is_game_over(board))
+})
+
+Deno.test("stalemate_queen", () => {
+    const board = position_from_fen("k7/2Q5/1K6/8/8/8/8/8 b - - 0 1")
+    assert(is_stalemate(board))
+    assert(is_game_over(board))
 })
