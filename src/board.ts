7c6d42bc4f6388be1b325aca1a80d729c5283c90
swanis <me@swan.is>
2023-02-22 19:30:29 +0100

    move is_square_controlled_by() to board.ts

--- src/board.ts
+++ src/board.ts
@@ -1,5 +1,6 @@
-import { coordinates_eq } from "./coordinates.ts";
-import { BoardPiece,Coordinates,BoardState,Color, Piece } from "./game_types.ts";
+import { coordinates_eq, make_coordinates } from "./coordinates.ts";
+import { BoardPiece, Coordinates, BoardState, Color, Piece, Move } from "./game_types.ts";
+import { is_check, get_piece_moves } from "./moves.ts";
 
 export function get_piece_by_square(coordinates: Coordinates, state: BoardState): BoardPiece | null {
     return state.pieces.find(
@@ -59,4 +60,41 @@ export function is_pawn(piece: BoardPiece): boolean {
 
 export function out_of_bounds(state: BoardState, coordinates: Coordinates) {
     return coordinates.x < 1 || coordinates.x > state.width || coordinates.y < 1 || coordinates.y > state.height
+}
+
+export function is_square_controlled_by(state: BoardState, square: Coordinates, color: Color) {
+    function squares_controlled_by_piece(board_state: BoardState, piece: BoardPiece): Coordinates[] {
+        const new_state = structuredClone(board_state)
+        new_state.pieces = new_state.pieces.filter((p: BoardPiece) => !coordinates_eq(p.square, piece.square))
+
+        if (piece.piece !== Piece.King && is_check(new_state, board_state.turn)) {
+            return []
+        }
+
+        if (piece.piece === Piece.Pawn) {
+            const first_pawn_square = make_coordinates(
+                piece.square.x - 1,
+                color === Color.White ? piece.square.y + 1 : piece.square.y - 1
+            )
+            const second_pawn_square = make_coordinates(
+                piece.square.x + 1,
+                color === Color.White ? piece.square.y + 1 : piece.square.y - 1
+            )
+            return [first_pawn_square, second_pawn_square]
+        }
+
+        if (piece.piece === Piece.King) {
+            return get_piece_moves(piece, board_state).filter((m: Move) => !(m.is_castling_kingside || m.is_castling_queenside)).map((m: Move) => m.to)
+        }
+
+        return get_piece_moves(piece, board_state).map((m: Move) => m.to)
+    }
+
+    if (color === state.turn) {
+        return get_player_pieces(state, state.turn).flatMap(piece => squares_controlled_by_piece(state, piece)).some(controlled_square => coordinates_eq(controlled_square, square))
+    } else {
+        const other_color_state = structuredClone(state)
+        other_color_state.turn = other_color(state.turn)
+        return get_player_pieces(other_color_state, other_color_state.turn).flatMap(piece => squares_controlled_by_piece(state, piece)).some(controlled_square => coordinates_eq(controlled_square, square))
+    }
 }
