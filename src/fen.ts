acc0d6fb7ed20c84264b1fffa875d79cec2d4d16
swanis <me@swan.is>
2023-03-02 01:30:02 +0100

    Document fen.ts

--- src/fen.ts
+++ src/fen.ts
@@ -2,6 +2,12 @@ import { BoardPiece, BoardState, Color, get_piece_by_square } from "./board.ts";
 import { coordinates_from_notation, coordinates_to_notation, make_coordinates } from "./coordinates.ts";
 import { get_color_by_letter, get_letter_by_piece, get_piece_by_letter } from "./notation.ts";
 
+/**
+ * Convert a FEN string to a BoardState
+ * @param FEN - the FEN string
+ * @precondition FEN is of valid FEN format
+ * @returns a BoardState representing the FEN string
+ */
 export function position_from_fen(FEN: string): BoardState {
     function get_pieces(piece_data: string): BoardPiece[] {
         let y = 8
@@ -44,7 +50,11 @@ export function position_from_fen(FEN: string): BoardState {
     }
 }
 
-
+/**
+ * Convert a BoardState to a FEN string
+ * @param state - the BoardState to convert
+ * @returns a FEN string representing the BoardState
+ */
 export function export_to_fen(state: BoardState): string {
     function row_to_fen(y: number): string {
         return [1, 2, 3, 4, 5, 6, 7, 8].map(
